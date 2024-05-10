85730c58d4e183d1c2fe89ab5faf5e0498cc2a73
dillonwhitener <djwhitener@gmail.com>
2024-05-10 11:38:45 -0500

    Committing Functiional code before compressing Table helper functions to a ternary statement and applying CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
+import Banner from "./components/Banner";
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
 
+import { useState } from 'react'
+import satData from "./components/satData"
+
 
 function App() {
+    const [sat, setSat] = useState(satData);
+    const orbitTypes = [...new Set(satData.map((data) => data.orbitType))];
+    const filterByType = (currentType) => {
+      const displaySats = satData.filter((newSatDisplay) => {
+         return newSatDisplay.orbitType === currentType;
+      });
+      setSat(displaySats);
+   };
+    // code continues ...
   return (
     <div>
-      <Buttons />
-      <Table />
+      <Banner />
+      <Buttons 
+      filterByType={filterByType}
+      setSat={setSat}
+      orbitTypes={orbitTypes}
+     />
+      <Table sat={sat}/>
     </div>
   );
 }
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,17 +1,52 @@
 const Table = ({ sat }) => {
   return (
     <table>
-    <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
-     </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
-   </table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {sat.map((data, idx) => {
+
+          function True() {
+            return (
+              <td>active</td>
+            );
+          }
+
+          function False() {
+            return (
+              <td>inactive</td>
+            );
+          }
+
+          function operationalStatus() {
+            if (data.operational === true) {
+              return (
+                <True />
+              );
+            }
+            return (
+              <False />
+            );
+          }
+
+          return (
+            <tr key={idx}>
+              <td>{data.name}</td>
+              <td>{data.type}</td>
+              <td>{data.launchDate}</td>
+              {operationalStatus()}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
   );
 };
 
